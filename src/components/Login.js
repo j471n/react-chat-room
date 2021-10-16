f85f748b92d0a1a47d4f1923caf8263e78e53c72
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-16 13:31:21 +0530

    Adding image some elements

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,24 @@ import React, { useState } from "react";
 import { auth, googleProvider } from "../services/firebase";
 import Loading from "./Loading/Loading";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const Login = () => {
   const [loading, setLoading] = useState(false);
   async function signInWithGoogle() {
     setLoading(true);
-    try {
-      await auth.signInWithPopup(googleProvider).then((res) => {
+
+    await auth
+      .signInWithPopup(googleProvider)
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(err.message);
         setLoading(false);
       });
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
   }
 
   if (loading) return <Loading />;
@@ -29,13 +35,15 @@ const Login = () => {
           className="flex items-center w-9/12 sm:w-96 py-2 px-4 m-2 bg-gray-200 rounded-md  font-medium shadow-md lg:hover:bg-blue-500 lg:hover:text-white dark:text-black"
           onClick={signInWithGoogle}
         >
-          <img src={"https://rb.gy/dgz5ha"} width={30} height={30} alt=""/>
+          <img src={"https://rb.gy/dgz5ha"} width={30} height={30} alt="" />
           <p className="text-center w-full">Login with Google</p>
         </button>
 
         {/* absolute thing */}
         <div className="w-56 h-56 md:w-96 md:h-96 rounded-tr-full rounded-br-full absolute bg-blue-400 -top-40 -left-10 blur-sm shadow-sm "></div>
         <div className="w-56 h-56 md:w-96 md:h-96 rounded-tl-full rounded-bl-full absolute bg-blue-400 -bottom-20 -right-10 blur-sm shadow-sm "></div>
+
+        <ToastContainer theme="dark" />
       </div>
     </>
   );
